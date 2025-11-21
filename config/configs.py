ce4d8b977004e04dc7aa1ccdf9d15986e8393f79
HASHIRAMA21 <sodiaque806@gmail.com>
2025-11-21 13:06:54 +0100

    feat confiuration

--- config/configs.py
+++ config/configs.py
@@ -2,13 +2,13 @@ import os
 
 
 class Settings:
-    KEYCLOAK_BASE_URL = "https://lemur-10.cloud-iam.com/"
+    KEYCLOAK_BASE_URL = "https://lemur-10.cloud-iam.com/auth"
     KEYCLOAK_REALM = "orange"
     KEYCLOAK_CLIENT_ID = "admin-cli"
     KEYCLOAK_CLIENT_SECRET = ""
     KEYCLOAK_USERNAME = "admin"
     KEYCLOAK_PASSWORD = "d0/Dv@?k/[#Hhl,A3RSL"
-    API_BASE_URL = "https://lemur-10.cloud-iam.com/"
+    API_BASE_URL = "https://lemur-10.cloud-iam.com/auth"
 
 settings = Settings()
 
